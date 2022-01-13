832d197cef090688b88ff13c869b43ff7a69d7a6
Renata Nitzsche <renata.nitzsche@hotmail.com>
2022-01-13 00:06:29 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/repositorie/MovieRepository.java
+++ backend/src/main/java/com/devsuperior/dsmovie/repositorie/MovieRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsmovie.entities.Movie;
 
 public interface MovieRepository extends JpaRepository  <Movie, Long>{
+	
 
 }
