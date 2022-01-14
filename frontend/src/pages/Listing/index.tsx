ac60b12c60cdbb4e986bf46e4adca5069ee536f1
Renata Nitzsche <renata.nitzsche@hotmail.com>
2022-01-14 14:29:25 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,20 @@
 
 
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+
 
 function Listing() {
+
+    //FORMA ERRADA
+ 
+      axios.get('http://localhost:8080/movies?size=12&page=0')
+     // axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data)
+    })
 
     return (
         <>
@@ -11,19 +22,19 @@ function Listing() {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div> 
-                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
                 </div>
